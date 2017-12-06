239b9018ab5b1581952af0625c2fa444a9deb8db
zhangkesheng <clark.zhang@mingyizhudao.com>
2017-12-06 11:04:40 +0800

    修改环境变量获取方式

--- src/main/java/myzd/config/TransferDefaultConfiguration.java
+++ src/main/java/myzd/config/TransferDefaultConfiguration.java
@@ -1,30 +1,39 @@
 package myzd.config;
 
 import okhttp3.OkHttpClient;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 
 @Configuration
 public class TransferDefaultConfiguration {
+
+  @Autowired
+  private Environment environment;
+
+  private final static String EXECUTOR_SET_CORE_POOL_SIZE = "executor.set.core.pool.size";
+  private final static String EXECUTOR_SET_MAX_POOL_SIZE = "executor.set.max.pool.size";
+  private final static String EXECUTOR_SET_QUEUE_CAPACITY = "executor.set.queue.capacity";
+
   @Bean
   public OkHttpClient okHttpClient() {
     return new OkHttpClient.Builder().build();
   }
 
   @Bean
-  public TaskExecutor kafkaMsgExecutor(
-    @Value("${executor.set.core.pool.size}")String corePoolSize,
-    @Value("${executor.set.max.pool.size}")String maxPoolSize,
-    @Value("${executor.set.queue.capacity}")String queueCapacity
-    ) {
+  public TaskExecutor kafkaMsgExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(Integer.parseInt(corePoolSize));
-    executor.setMaxPoolSize(Integer.parseInt(maxPoolSize));
-    executor.setQueueCapacity(Integer.parseInt(queueCapacity));
+    int corePoolSize = StringUtils.isBlank(environment.getProperty(EXECUTOR_SET_CORE_POOL_SIZE)) ? 5 : Integer.parseInt(environment.getProperty(EXECUTOR_SET_CORE_POOL_SIZE));
+    int maxPoolSize = StringUtils.isBlank(environment.getProperty(EXECUTOR_SET_MAX_POOL_SIZE)) ? 10 : Integer.parseInt(environment.getProperty(EXECUTOR_SET_MAX_POOL_SIZE));
+    int queueCapacity = StringUtils.isBlank(environment.getProperty(EXECUTOR_SET_QUEUE_CAPACITY)) ? 25 : Integer.parseInt(environment.getProperty(EXECUTOR_SET_QUEUE_CAPACITY));
+    executor.setCorePoolSize(corePoolSize);
+    executor.setMaxPoolSize(maxPoolSize);
+    executor.setQueueCapacity(queueCapacity);
     return executor;
   }
 }
