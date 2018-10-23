b535aa9ecc07ef672251cae1d68bdddafad07381
刘钱 <qian.liu@mingyizhudao.com>
2018-10-23 15:36:07 +0800

    :rocket: fix redis connect bug and make the project runnable.

--- lib-edge-core/src/main/java/libedge/config/LibEdgeAuthorizationConfiguration.java
+++ lib-edge-core/src/main/java/libedge/config/LibEdgeAuthorizationConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -28,122 +27,128 @@ import javax.sql.DataSource;
  * @author yrw
  * @since 2018/2/18
  */
-
 @Slf4j
 @Configuration
 public class LibEdgeAuthorizationConfiguration {
 
-	private final String DATASOURCE_URL = "${authorization.datasource.url}";
-	private final String DATASOURCE_USERNAME = "${authorization.datasource.username}";
-	private final String DATASOURCE_PASSWORD = "${authorization.datasource.password}";
-	private final String DATASOURCE_DRIVER_CLASS_NAME = "${authorization.datasource.driverClassName}";
+  private final String DATASOURCE_URL = "${authorization.datasource.url}";
+  private final String DATASOURCE_USERNAME = "${authorization.datasource.username}";
+  private final String DATASOURCE_PASSWORD = "${authorization.datasource.password}";
+  private final String DATASOURCE_DRIVER_CLASS_NAME = "${authorization.datasource.driverClassName}";
 
-	private final String REDIS_HOST_NAME = "${authorization.redis.host}";
-	private final String REDIS_PASSWORD = "${authorization.redis.password}";
-	private final String REDIS_PORT = "${authorization.redis.port}";
-	private final String REDIS_MAX_ACTIVE = "${authorization.redis.pool.max-active}";
-	private final String REDIS_MAX_IDLE = "${authorization.redis.pool.max-idle}";
-	private final String REDIS_MIN_IDLE = "${authorization.redis.pool.min-idle}";
-	private final String REDIS_MAX_WAIT = "${authorization.redis.pool.max-wait}";
+  private final String REDIS_HOST_NAME = "${authorization.redis.host}";
+  private final String REDIS_PASSWORD = "${authorization.redis.password}";
+  private final String REDIS_PORT = "${authorization.redis.port}";
+  private final String REDIS_MAX_ACTIVE = "${authorization.redis.pool.max-active}";
+  private final String REDIS_MAX_IDLE = "${authorization.redis.pool.max-idle}";
+  private final String REDIS_MIN_IDLE = "${authorization.redis.pool.min-idle}";
+  private final String REDIS_MAX_WAIT = "${authorization.redis.pool.max-wait}";
 
-	@Bean
-	public DataSource libEdgeAuthorizationDateSource(
-					@Value(DATASOURCE_URL) String url,
-					@Value(DATASOURCE_USERNAME) String username,
-					@Value(DATASOURCE_PASSWORD) String password,
-					@Value(DATASOURCE_DRIVER_CLASS_NAME) String driverClassName
-	) {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
-		dataSource.setDriverClassName(driverClassName);
-		return dataSource;
-	}
+  @Bean
+  public DataSource libEdgeAuthorizationDateSource(
+    @Value(DATASOURCE_URL) String url,
+    @Value(DATASOURCE_USERNAME) String username,
+    @Value(DATASOURCE_PASSWORD) String password,
+    @Value(DATASOURCE_DRIVER_CLASS_NAME) String driverClassName
+  ) {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
+    dataSource.setDriverClassName(driverClassName);
+    return dataSource;
+  }
 
-	@Bean
-	@Autowired
-	public JdbcTemplate libEdgeAuthorizationJdbcTemplate(
-					@Qualifier("libEdgeAuthorizationDateSource") DataSource dataSource) {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-		jdbcTemplate.setDataSource(dataSource);
-		return jdbcTemplate;
-	}
+  @Bean
+  @Autowired
+  public JdbcTemplate libEdgeAuthorizationJdbcTemplate(
+    @Qualifier("libEdgeAuthorizationDateSource") DataSource dataSource) {
+    JdbcTemplate jdbcTemplate = new JdbcTemplate();
+    jdbcTemplate.setDataSource(dataSource);
+    return jdbcTemplate;
+  }
 
-	@Bean
-	public RedisTemplate libEdgeAuthorizationRedisTemplate(
-					@Value(REDIS_HOST_NAME) String hostName,
-					@Value(REDIS_PASSWORD) String password,
-					@Value(REDIS_PORT) int port,
-					@Value(REDIS_MAX_ACTIVE) String maxActive,
-					@Value(REDIS_MAX_IDLE) String maxIdle,
-					@Value(REDIS_MIN_IDLE) String minIdle,
-					@Value(REDIS_MAX_WAIT) String maxWait
-	) {
-		JedisPoolConfig poolConfig = new JedisPoolConfig();
-		if (StringUtils.isNotBlank(minIdle)) {
-			poolConfig.setMinIdle(Integer.valueOf(minIdle));
-		}
-		if (StringUtils.isNotBlank(maxIdle)) {
-			poolConfig.setMaxIdle(Integer.valueOf(maxIdle));
-		}
-		if (StringUtils.isNotBlank(maxActive)) {
-			poolConfig.setMaxTotal(Integer.valueOf(maxActive));
-		}
-		if (StringUtils.isNotBlank(maxWait)) {
-			poolConfig.setMaxWaitMillis(Integer.valueOf(maxWait));
-		}
-		RedisSentinelConfiguration sentinelConfiguration = new RedisSentinelConfiguration().sentinel(hostName, port);
-		sentinelConfiguration.setPassword(RedisPassword.of(password));
-		JedisConnectionFactory factory = new JedisConnectionFactory(sentinelConfiguration, poolConfig);
-		factory.afterPropertiesSet();
+  @Bean
+  public RedisTemplate libEdgeAuthorizationRedisTemplate(
+    @Value(REDIS_HOST_NAME) String hostName,
+    @Value(REDIS_PASSWORD) String password,
+    @Value(REDIS_PORT) int port,
+    @Value(REDIS_MAX_ACTIVE) String maxActive,
+    @Value(REDIS_MAX_IDLE) String maxIdle,
+    @Value(REDIS_MIN_IDLE) String minIdle,
+    @Value(REDIS_MAX_WAIT) String maxWait
+  ) {
+    JedisPoolConfig poolConfig = new JedisPoolConfig();
+    if (StringUtils.isNotBlank(minIdle)) {
+      poolConfig.setMinIdle(Integer.valueOf(minIdle));
+    }
+    if (StringUtils.isNotBlank(maxIdle)) {
+      poolConfig.setMaxIdle(Integer.valueOf(maxIdle));
+    }
+    if (StringUtils.isNotBlank(maxActive)) {
+      poolConfig.setMaxTotal(Integer.valueOf(maxActive));
+    }
+    if (StringUtils.isNotBlank(maxWait)) {
+      poolConfig.setMaxWaitMillis(Integer.valueOf(maxWait));
+    }
 
-		RedisSerializer serializer = new StringRedisSerializer();
-		RedisTemplate redisTemplate = new RedisTemplate();
-		redisTemplate.setConnectionFactory(factory);
-		redisTemplate.setKeySerializer(serializer);
-		redisTemplate.setValueSerializer(serializer);
-		return redisTemplate;
-	}
+    JedisClientConfiguration.JedisClientConfigurationBuilder builder = JedisClientConfiguration.builder();
+    builder.usePooling().poolConfig(poolConfig);
 
-	@Bean
-	@Autowired
-	public RoleRepository libEdgeRoleRepository(
-					@Qualifier("libEdgeAuthorizationJdbcTemplate") JdbcTemplate jdbcTemplate,
-					@Qualifier("libEdgeAuthorizationRedisTemplate") RedisTemplate redisTemplate) {
-		return new RoleRepository(jdbcTemplate, redisTemplate);
-	}
+    RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration();
+    standaloneConfiguration.setHostName(hostName);
+    standaloneConfiguration.setPassword(RedisPassword.of(password));
+    standaloneConfiguration.setPort(port);
 
-	@Bean
-	@Autowired
-	public UserDetailsServiceImpl userDetailsServiceImpl(
-					@Qualifier("libEdgeRoleRepository") RoleRepository roleRepository) {
-		return new UserDetailsServiceImpl(roleRepository);
-	}
+    JedisConnectionFactory factory = new JedisConnectionFactory(standaloneConfiguration, builder.build());
+    factory.afterPropertiesSet();
 
-	@Bean
-	public RedisTemplate libEdgeSessionRedisTemplate(
-					@Value(REDIS_HOST_NAME) String hostName,
-					@Value(REDIS_PASSWORD) String password,
-					@Value(REDIS_PORT) int port
-	) {
-		RedisStandaloneConfiguration envConfiguration = new RedisStandaloneConfiguration();
-		envConfiguration.setHostName(hostName);
-		envConfiguration.setPort(port);
-		envConfiguration.setPassword(RedisPassword.of(password));
-		JedisConnectionFactory factory = new JedisConnectionFactory(envConfiguration, JedisClientConfiguration.defaultConfiguration());
-		factory.afterPropertiesSet();
+    RedisSerializer serializer = new StringRedisSerializer();
+    RedisTemplate redisTemplate = new RedisTemplate();
+    redisTemplate.setConnectionFactory(factory);
+    redisTemplate.setKeySerializer(serializer);
+    redisTemplate.setValueSerializer(serializer);
+    return redisTemplate;
+  }
 
-		RedisTemplate redisTemplate = new RedisTemplate();
-		redisTemplate.setConnectionFactory(factory);
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
-		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-		redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-		return redisTemplate;
-	}
+  @Bean
+  @Autowired
+  public RoleRepository libEdgeRoleRepository(
+    @Qualifier("libEdgeAuthorizationJdbcTemplate") JdbcTemplate jdbcTemplate,
+    @Qualifier("libEdgeAuthorizationRedisTemplate") RedisTemplate redisTemplate) {
+    return new RoleRepository(jdbcTemplate, redisTemplate);
+  }
 
-	@Bean
-	@Autowired
-	public SessionCacheService libEdgeSessionCacheService(
-					@Qualifier("libEdgeSessionRedisTemplate") RedisTemplate redisTemplate) {
-		return new SessionCacheService(redisTemplate);
-	}
+  @Bean
+  @Autowired
+  public UserDetailsServiceImpl userDetailsServiceImpl(
+    @Qualifier("libEdgeRoleRepository") RoleRepository roleRepository) {
+    return new UserDetailsServiceImpl(roleRepository);
+  }
+
+  @Bean
+  public RedisTemplate libEdgeSessionRedisTemplate(
+    @Value(REDIS_HOST_NAME) String hostName,
+    @Value(REDIS_PASSWORD) String password,
+    @Value(REDIS_PORT) int port
+  ) {
+    RedisStandaloneConfiguration envConfiguration = new RedisStandaloneConfiguration();
+    envConfiguration.setHostName(hostName);
+    envConfiguration.setPort(port);
+    envConfiguration.setPassword(RedisPassword.of(password));
+    JedisConnectionFactory factory = new JedisConnectionFactory(envConfiguration, JedisClientConfiguration.defaultConfiguration());
+    factory.afterPropertiesSet();
+
+    RedisTemplate redisTemplate = new RedisTemplate();
+    redisTemplate.setConnectionFactory(factory);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+    return redisTemplate;
+  }
+
+  @Bean
+  @Autowired
+  public SessionCacheService libEdgeSessionCacheService(
+    @Qualifier("libEdgeSessionRedisTemplate") RedisTemplate redisTemplate) {
+    return new SessionCacheService(redisTemplate);
+  }
 }
